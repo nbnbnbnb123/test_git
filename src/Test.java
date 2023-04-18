ea54c29bce75d1dab395763d610049db92e6e240
wangdanniao <3066026384@qq.com>
2023-04-18 14:15:27 +0800

    新增内容

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test.class");
         System.out.println("新增内容");
+        System.out.println("第二次增加内容");
     }
 }
